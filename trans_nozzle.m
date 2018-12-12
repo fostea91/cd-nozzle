273392db15a5503d0f0d8b2c6a69deb9c46674bc
fostea91 <fost22@protonmail.com>
2018-12-12 08:35:53 -0800

    Inserted copyright and license notices.

--- trans_nozzle.m
+++ trans_nozzle.m
@@ -1,3 +1,24 @@
+% trans_nozzle.m
+% 
+% Copyright 2018 Andrew Foster <fost22@protonmail.com>
+%
+% Flow analysis in the transonic region of a convergent-divergent nozzle
+% with a circular, parabolic, or rectangular hyperbolic convergent contour,
+% based to the work of Dutton and Addy, 1981
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 function M = trans_nozzle(nu, x, y, gm, expan)
 
 z = (((gm+1)*expan*0.5)^(-0.5))*x;
--- transonic_nozzle.m
+++ transonic_nozzle.m
@@ -1,9 +1,23 @@
 % transonic_nozzle.m
-% Author: Andrew Foster
+% 
+% Copyright 2018 Andrew Foster <fost22@protonmail.com>
 %
 % Flow analysis in the transonic region of a convergent-divergent nozzle
 % with a circular, parabolic, or rectangular hyperbolic convergent contour,
 % based to the work of Dutton and Addy, 1981
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 clear;
 clc
